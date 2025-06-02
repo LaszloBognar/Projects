3c054676b8f2f24857630bc18b1ed5a74ee6d8bb
LaszloBognar <hoshikagee@gmail.com>
2025-06-02 20:58:10 +0200

    Delete Reverse_words.py

--- Reverse_words.py
+++
@@ -1,8 +0,0 @@
-def reverse_words(s: str) -> str:
-    words = s.strip().split()
-
-    reverse_sentence = ' '.join(words[::-1])
-    print(reverse_sentence)
-
-    return (reverse_sentence)
-reverse_words("The sky is blue")
